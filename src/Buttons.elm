8e9cf2ef35d57fa482172b4bd72108129e614f60
zsolt kormany <zsoltkormany@gmail.com>
2019-12-07 21:44:32 +0100

    first working version

---
+++ src/Buttons.elm
@@ -0,0 +1,76 @@
+module Main exposing (..)
+
+-- Press buttons to increment and decrement a counter.
+--
+-- Read how it works:
+--   https://guide.elm-lang.org/architecture/buttons.html
+--
+
+
+import Browser
+import Html exposing (Html, button, div, text)
+import Html.Events exposing (onClick)
+
+
+
+-- MAIN
+
+
+main =
+  Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model = Int
+
+
+init : Model
+init =
+  0
+
+
+
+-- UPDATE
+
+
+type Msg
+  = Increment
+  | IncrementByTen
+  | Decrement
+  | Reset
+
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Increment ->
+      model + 1
+
+    IncrementByTen ->
+      model + 10
+
+    Decrement ->
+      model - 1
+
+    Reset ->
+      0
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ button [ onClick Decrement ] [ text "-" ]
+    , div [] [ text (String.fromInt model) ]
+    , button [ onClick Increment ] [ text "+" ],
+    div []
+    [ button [ onClick IncrementByTen ] [ text "Increment by 10" ]
+    , button [ onClick Reset ] [ text "Reset" ]
+    ]
+    ]
---
+++ src/PhotoGroove.elm
@@ -0,0 +1,183 @@
+module PhotoGroove exposing (main)
+
+import Array exposing (Array)
+import Browser
+import Html exposing (Html, h1, h3, div, img, button, label, text, input)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Http
+import Json.Decode
+import Json.Decode.Pipeline
+import Random
+
+urlPrefix : String
+urlPrefix =
+  "http://elm-in-action.com/"
+
+type Msg
+    = ClickedPhoto String
+    | ClickedSize ThumbnailSize
+    | ClickedSurpriseMe
+    | GotRandomPhoto Photo
+    | GotPhotos (Result Http.Error (List Photo))
+
+view : Model -> Html Msg
+view model =
+  div [ class "content" ] <|
+      case model.status of
+        Loaded photos selectedUrl ->
+          viewLoaded photos selectedUrl model.chosenSize
+
+        Loading ->
+          []
+
+        Errored errorMessage ->
+          [text ("Error: " ++ errorMessage)]
+
+viewLoaded : List Photo -> String -> ThumbnailSize -> List (Html Msg)
+viewLoaded photos selectedUrl chosenSize =
+  [ h1 [] [ text "Photo Groove" ]
+  , button
+    [ onClick ClickedSurpriseMe ]
+    [ text "Surprise Me!" ]
+  , h3 [] [ text "Thumbnail Size:" ]
+  , div [ id "choose-size" ]
+    (List.map viewSizeChooser [ Small, Medium, Large ])
+  , div [ id "thumbnails", class (sizeToString chosenSize) ]
+    (List.map (viewThumbnail selectedUrl) photos)
+  , img
+    [ class "large"
+    , src (urlPrefix ++ "large/" ++ selectedUrl)
+    ]
+    []
+  ]
+
+viewThumbnail : String -> Photo -> Html Msg
+viewThumbnail selectedUrl thumb =
+  img
+    [ src (urlPrefix ++ thumb.url)
+    , title (thumb.title ++ " [" ++ String.fromInt thumb.size ++ " KB]")
+    , classList [ ( "selected", selectedUrl == thumb.url ) ]
+    , onClick (ClickedPhoto thumb.url)
+    ]
+    []
+
+viewSizeChooser : ThumbnailSize -> Html Msg
+viewSizeChooser size =
+  label []
+    [ input [ type_ "radio", name "size", onClick (ClickedSize size) ] []
+    , text  (sizeToString size)
+    ]
+
+sizeToString : ThumbnailSize -> String
+sizeToString size =
+  case size of
+    Small ->
+      "small"
+
+    Medium ->
+      "med"
+
+    Large ->
+      "large"
+
+type ThumbnailSize
+  = Small
+  | Medium
+  | Large
+
+type alias Photo =
+  { url : String
+  , size : Int
+  , title : String
+  }
+
+photoDecoder : Json.Decode.Decoder Photo
+photoDecoder =
+  Json.Decode.succeed Photo
+    |> Json.Decode.Pipeline.required "url" Json.Decode.string
+    |> Json.Decode.Pipeline.required "size" Json.Decode.int
+    |> Json.Decode.Pipeline.optional "title" Json.Decode.string "(untitled)"
+
+type Status
+  = Loading
+  | Loaded (List Photo) String
+  | Errored String
+
+type alias Model =
+  { status : Status
+  , chosenSize : ThumbnailSize
+  }
+
+initialModel : Model
+initialModel =
+  { status = Loading
+  , chosenSize = Medium
+  }
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+  case msg of
+    GotRandomPhoto photo ->
+      ( { model | status = selectUrl photo.url model.status }, Cmd.none )
+
+    ClickedPhoto url ->
+      ( { model | status = selectUrl url model.status }, Cmd.none )
+
+    ClickedSize size ->
+      ( { model | chosenSize = size }, Cmd.none )
+
+    ClickedSurpriseMe ->
+      case model.status of
+        Loaded (firstPhoto :: otherPhotos) _ ->
+          Random.uniform firstPhoto otherPhotos
+            |> Random.generate GotRandomPhoto
+            |> Tuple.pair model
+
+        Loaded [] _ ->
+          ( model, Cmd.none )
+
+        Loading ->
+          ( model, Cmd.none )
+
+        Errored errorMessage ->
+          ( model, Cmd.none )
+
+    GotPhotos (Ok photos) ->
+      case photos of
+        first :: rest ->
+          ( { model | status = Loaded photos first.url }, Cmd.none )
+
+        [] ->
+          ( { model | status = Errored "No photos found" }, Cmd.none )
+
+    GotPhotos (Err httpError) ->
+      ( { model | status = Errored "Server error" }, Cmd.none )
+
+selectUrl : String -> Status -> Status
+selectUrl url status =
+  case status of
+    Loaded photos _ ->
+      Loaded photos url
+
+    Loading ->
+      status
+
+    Errored errorMessage ->
+      status
+
+initialCmd : Cmd Msg
+initialCmd =
+  Http.get
+    { url = urlPrefix ++ "photos/list.json"
+    , expect = Http.expectJson GotPhotos (Json.Decode.list photoDecoder)
+    }
+
+main : Program () Model Msg
+main =
+  Browser.element
+  { init = \flags -> ( initialModel, initialCmd )
+    , view = view
+    , update = update
+    , subscriptions = \_ -> Sub.none
+  }
